01787aae33d741268a5c02e3368f59c6a90e6db8
Yaswanth Chandra, D <d.yaswanth.chandra@accenture.com>
2019-08-02 17:17:06 +0530

    List Changes

--- MyBusinessDomain/BusinessDomain/DisplayEmployee.cs
+++ MyBusinessDomain/BusinessDomain/DisplayEmployee.cs
@@ -17,7 +17,10 @@ namespace MyBusinessDomain.BusinessDomain
 
         public List<EmployeeDetails> ShowEmployeeDetails()
         {
-            List<EmployeeDetails> details = new List<EmployeeDetails>();
+            List<EmployeeDetails> details = new List<EmployeeDetails>() {
+                new EmployeeDetails {eid = 100, ename = "Yaswanth", salary = 25000.23m }
+            };
+
             details.Add(new EmployeeDetails { eid = 101, ename = "Yaswanth", salary = 25000.23m });
             details.Add(new EmployeeDetails { eid = 102, ename = "Yash", salary = 852563.23m });
             details.Add(new EmployeeDetails { eid = 103, ename = "Prash", salary = 12559.23m });
